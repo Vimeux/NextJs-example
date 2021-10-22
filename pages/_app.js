a9b6df2574ca2c620dd8bdb2d07ca031cd680635
Vimeux <vimeux.b@hotmail.com>
2021-10-22 23:06:45 +0200

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+/**
+ * Cette page permet d'avoir du style en global qui affectera tous le site
+ */
+import '../styles/global.css'
+
+export default function App ({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
